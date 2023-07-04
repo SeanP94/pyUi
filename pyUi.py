d6c2b6bb876b2b138b91f638639bc1485482274a
Sean <seanep94@gmail.com>
2023-07-03 18:13:37 -0700

    General Pygame screen layout stuff

--- pyUi.py
+++ pyUi.py
@@ -1,5 +1,19 @@
 import pygame as pg
 
+
 SCREEN_WIDTH = 400
 SCREEN_HEIGHT = 400
+
+pg.init()
+
+# Get the screen Surface and the clock to limit fps.
+screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pg.time.Clock()
+RUN_FLAG = True
+
+while RUN_FLAG:
+
+
+    clock.tick(60) # Cap 60FPS
+
 
