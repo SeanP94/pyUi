f993f2dbc0bb0ddcd3a507a3b289a211fb91d29b
seanperry <seanep94@gmail.com>
2023-07-05 13:48:32 -0700

    Made a little ball click and move program

--- pyUi.py
+++ pyUi.py
@@ -9,20 +9,34 @@ sys.path.insert(1, str(cpath/'logger'))
 import colors as cls
 from errLog import logError
 
+import random
 
 # Temp Globals
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-
+FPS = 60 
 pg.init()
  
 # # # # # X
 # Temp    #
 # Globals # 
 # # # # # #
+BALLH = 100
 
+MAX_X = SCREEN_HEIGHT - BALLH
+MAX_Y = SCREEN_WIDTH - BALLH
+
+ballx = random.randrange(MAX_X)
+bally = random.randrange(MAX_Y)
 ballImage = pg.image.load(cpath / 'images/ball.png')
+ballRect = pg.Rect(ballx, bally, BALLH, BALLH)
 
+def resetBall():
+    global ballx, bally, ballRect
+    ballx = random.randrange(MAX_X)
+    bally = random.randrange(MAX_Y)
+    ballRect = pg.Rect(ballx, bally, BALLH, BALLH)
+    
 
 # # # # # X
 
@@ -46,6 +60,11 @@ def eventHandler():
         else:
             window.fill(cls.LIGHT_GRAY)
 
+        
+        if event.type == pg.MOUSEBUTTONDOWN:
+            if ballRect.collidepoint(event.pos):
+                print(f"{event.pos} : Booped the ball")
+                resetBall()
 def update():
     '''Placeholder for now, does nothing'''
     pass
@@ -54,7 +73,7 @@ def update():
 def draw():
     # LIGHT_GRAY is essentially our "clear" the screen color.
     window.fill(cls.LIGHT_GRAY)
-    window.blit(ballImage, (0,0))
+    window.blit(ballImage, (ballx,bally))
 
 
 def gameLoop():
@@ -66,9 +85,9 @@ def gameLoop():
         eventHandler()
         update()
         draw()
-
         pg.display.update() # Update the window.
-        clock.tick(60) # Cap 60FPS
+        clock.tick(FPS) # Cap 60FPS
+
 
 
 # This file wont always be main. But it will run this as a test.
