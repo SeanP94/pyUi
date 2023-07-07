d2f8d01814a44e1f44ff0a2afd168b775f315b4c
Sean <seanep94@gmail.com>
2023-07-07 11:30:10 -0700

    Began extracting 2D vector functions from my test ball game classes

--- src/ballGame.py
+++ src/ballGame.py
@@ -2,6 +2,84 @@ import pygame as pg
 import random
 import pygameglobals as g
 import math
+
+### Functions that need to be exported eventually into a 2D Movement class.
+
+def confineBoundaries(rect : pg.rect.Rect):
+    '''
+    Keeps objects rect within screen bounadires.
+    '''
+    if rect.right > g.SCREEN_WIDTH:
+        rect.right = g.SCREEN_WIDTH
+    elif rect.left < 0:
+        rect.left = 0
+    if rect.top < 0:
+        rect.top = 0
+    elif rect.bottom > g.SCREEN_HEIGHT:
+        rect.bottom = g.SCREEN_HEIGHT
+
+def normalize(moveList):
+    '''
+    Normalizes a 2D vector so that if it's moving 2 directions
+    both values will be half their current amount. 
+    
+    IE we want it to go left, itll be at a ratio 0f 1
+    but if we go left and down itll bea ratio of .5 and .5 of each
+    direction
+    '''
+    if 0 not in moveList:         
+        moveList[0] *= .5
+        moveList[1] *= .5
+
+def moveCenterTowardsPoint(position : tuple[int, int], rectObj : pg.rect.Rect, speed : int, radius=1):
+    '''
+    Moves rectObj towards position. Within a radius.
+
+    Keyword arguments:
+    positon -- x, y tuple usually a mouse pos.
+    rectObj -- the Rectangle object we are moving the center
+        of towards position
+    radius -- This is to prevent object jitters once it gets close.
+        it'll stop within {radius} amount of pixels. Should always
+        be at least 1 since mouse location is a little silly and
+        can be between pixels*
+    '''
+    moveList = []
+    # Iterate over both tuples. To get directions.
+    for pos, myObj in zip(position, rectObj.center):
+        # 0 if point is within radius
+        if (myObj - pos) < radius and (myObj - pos) > -radius:
+            i = 0
+        else: # Else if value needs to go -1 or 1
+            i = 1 if (myObj - pos) < 0 else -1
+        moveList.append(i) # Adds -1, 0, or 1
+    # Normalize the 2D vector.
+    normalize(moveList)
+    # Add the need for speed.
+    moveList[0] *= speed
+    moveList[1] *= speed
+    
+    rectObj.left += math.floor(moveList[0])
+    rectObj.top += math.floor(moveList[1])
+    
+    # Keep rect within boundaries.
+    confineBoundaries(rect=rectObj)
+
+def randomPlacement(rectObj : pg.rect.Rect, maxX : int, maxY : int):
+    """
+    Based on Top Left point, places this at a random point in the screen.
+
+    ** Note, make sure you g.SCREEN_HEIGHT - rectObj_ylen and g.SCREEN_WIDTH - rectObj_Xlen
+    first so it places it within screen boundaries **
+
+    Keyword arguments:
+    rectObj -- Rectangle to move randomly.
+    maxX -- CLEANED x coord that MUST be within screen
+    maxY -- CLEANED y coord that MUST be within screen
+    """
+    rectObj.left = random.randrange(maxX)
+    rectObj.top = random.randrange(maxY)
+
 class BallRand():
     def __init__(self, cpath):    
         self.BALLH = 100
@@ -17,8 +95,7 @@ class BallRand():
         """
         Resets the ball to a random location on the screen.
         """
-        self.ballRect.top = random.randrange(self.MAX_Y)
-        self.ballRect.left = random.randrange(self.MAX_X)
+        randomPlacement(self.ballRect, self.MAX_X, self.MAX_Y)
 
     def updateBall(self, event : pg.event.Event):
         """
@@ -48,8 +125,7 @@ class BallDrag():
         """
         Resets the ball to a random location on the screen.
         """
-        self.ballRect.top = random.randrange(self.MAX_Y)
-        self.ballRect.left = random.randrange(self.MAX_X)
+        randomPlacement(self.ballRect, self.MAX_X, self.MAX_Y)
 
     def dragBall(self, posTuple):
         '''
@@ -57,58 +133,21 @@ class BallDrag():
         Thats the goal of this class
 
         Since there is the issue that getting the 0's just right for the area.
-        We want to make sure the value is within a radius of 3
+        We want to make sure the value is within a radius of 5
+        
+        The radius should be controlled by the size of the pixels.
+        Note this for when building a real feature.
+
         '''
-
-        moveList = []
-        r = 3
-        # Iterate over both tuples.
-
-        for pos, ball in zip(posTuple, self.ballRect.center):
-            if (ball - pos) < r and (ball - pos) > -r:
-                i = 0
-            else:
-                i = 1 if (ball - pos) < 0 else -1
-            moveList.append(i)
-
-        # Normalize logic : Move this out later
-        
-        print(moveList)
-        def normalize(moveList):
-            if 0 not in moveList: 
-                #mag = abs(math.sqrt(moveList[0] ** 2 + moveList[1] ** 2))        
-                moveList[0] *= .5
-                moveList[1] *= .5
-        normalize(moveList)
-        
-        moveList[0] *= self.speed
-        moveList[1] *= self.speed
-        print(moveList)
-        self.ballRect.left += math.floor(moveList[0])# * self.speed
-        self.ballRect.top += math.floor(moveList[1])# * self.speed
-        
-        # Set boundaries
-        if self.ballRect.right > g.SCREEN_WIDTH:
-            self.ballRect.right = g.SCREEN_WIDTH
-        elif self.ballRect.left < 0:
-            self.ballRect.left = 0
-        if self.ballRect.top < 0:
-            self.ballRect.top = 0
-        elif self.ballRect.bottom > g.SCREEN_HEIGHT:
-            self.ballRect.bottom = g.SCREEN_HEIGHT
-
+        moveCenterTowardsPoint(posTuple, self.ballRect, self.speed, radius=5)
 
     def updateBall(self, event : pg.event.Event):
         """
         Takes in an event type and if the ball collides with the
         mouse, move it to a random location
         """
-        print(pg.mouse.get_pos())
         if event.type == pg.MOUSEBUTTONDOWN:
             self.dragBall(pg.mouse.get_pos())
-        # if event.type == pg.MOUSEBUTTONUP:
-        #     self.dragBall(event.pos, False)
-        
         self.dragBall(pg.mouse.get_pos())
 
     def drawBall(self, surface : pg.surface.Surface):
